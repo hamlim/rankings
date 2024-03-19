644ec7f9ade2fc4a9025457b74d379acf5e1cecc
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-03-19 18:42:42 -0400

    Get the code working probably!

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	server.Create(8080, func(ctx server.Context) (server.Response, error) { // Update the function signature
+	server.Create(8080, func(ctx server.Context) (server.Response, error) {
 		return server.Response{
 			StatusCode: 200,
 			Headers:    map[string]string{"Content-Type": "text/plain"},
